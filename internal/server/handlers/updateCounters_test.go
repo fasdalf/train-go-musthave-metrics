8e14c6160008881fe26ea48f942ea559e663dac6
E.Sitnikov <e.sitnikov@etpgpb.ru>
2024-06-30 15:16:41 +0300

    Iter 2 attempt 7

--- internal/server/handlers/updateCounters_test.go
+++ internal/server/handlers/updateCounters_test.go
@@ -19,18 +19,38 @@ func TestUpdateMetricHandler(t *testing.T) {
 	}
 	tests := []struct {
 		name string
+		url  string
 		args args
 		want want
 	}{
 		{
-			name: "single one",
+			name: "empty",
+			url:  "/",
 			args: args{metricstorage.NewMemStorage()},
 			want: want{statusCode: http.StatusNotFound, counters: 0, gauges: 0},
+		},
+		{
+			name: "gauge",
+			url:  "/gauge/some-metric/10.001",
+			args: args{metricstorage.NewMemStorage()},
+			want: want{statusCode: http.StatusOK, counters: 0, gauges: 1},
+		},
+		{
+			name: "counter",
+			url:  "/counter/some-metric/10",
+			args: args{metricstorage.NewMemStorage()},
+			want: want{statusCode: http.StatusOK, counters: 1, gauges: 0},
+		},
+		{
+			name: "NaN",
+			url:  "/counter/some-metric/NaN",
+			args: args{metricstorage.NewMemStorage()},
+			want: want{statusCode: http.StatusBadRequest, counters: 0, gauges: 0},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			r := httptest.NewRequest(http.MethodPost, "/", nil)
+			r := httptest.NewRequest(http.MethodPost, tt.url, nil)
 			w := httptest.NewRecorder()
 			handler := UpdateMetricHandler(tt.args.s)
 			handler.ServeHTTP(w, r)
