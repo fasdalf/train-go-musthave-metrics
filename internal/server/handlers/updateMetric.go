663960bcf2962538b19b1b671af597a922e62075
E.Sitnikov <e.sitnikov@etpgpb.ru>
2024-07-09 10:23:52 +0300

    Iter 5 attempt 2 fix codestyle

--- internal/server/handlers/updateMetric.go
+++ internal/server/handlers/updateMetric.go
@@ -21,7 +21,7 @@ func NewUpdateMetricHandler(s Storage) func(c *gin.Context) {
 
 		if mName == "" {
 			slog.Error("Metric not found")
-			_ = c.AbortWithError(http.StatusNotFound, errors.New("Metric not found"))
+			_ = c.AbortWithError(http.StatusNotFound, errors.New("metric not found"))
 			return
 		}
 
@@ -30,7 +30,7 @@ func NewUpdateMetricHandler(s Storage) func(c *gin.Context) {
 			floatValue, err := strconv.ParseFloat(mValue, 64)
 			if err != nil {
 				slog.Error("Invalid metric value, float64 required", `value`, html.EscapeString(mType))
-				_ = c.AbortWithError(http.StatusBadRequest, errors.New("Invalid metric value"))
+				_ = c.AbortWithError(http.StatusBadRequest, errors.New("invalid metric value"))
 				return
 			}
 			s.UpdateGauge(mName, floatValue)
@@ -39,7 +39,7 @@ func NewUpdateMetricHandler(s Storage) func(c *gin.Context) {
 			intValue, err := strconv.Atoi(mValue)
 			if err != nil {
 				slog.Error("Invalid metric value, integer required", `value`, html.EscapeString(mType))
-				_ = c.AbortWithError(http.StatusBadRequest, errors.New("Invalid metric value"))
+				_ = c.AbortWithError(http.StatusBadRequest, errors.New("invalid metric value"))
 				return
 			}
 			s.UpdateCounter(mName, intValue)
@@ -47,7 +47,7 @@ func NewUpdateMetricHandler(s Storage) func(c *gin.Context) {
 		default:
 			slog.Error("Type is invalid", "type", html.EscapeString(mType))
 			_ = c.AbortWithError(http.StatusBadRequest, fmt.Errorf(
-				`Invalid type "%s", only "%s" and "%s" are supported`,
+				`invalid type "%s", only "%s" and "%s" are supported`,
 				html.EscapeString(mType),
 				constants.GaugeStr,
 				constants.CounterStr,
